c52a671c642583c66d974630f3fc058e9eafbff0
NgigiN <ngigi.nyongo@gmail.com>
2023-12-01 22:53:10 +0300

    fixing eslinting issues

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,12 +1,12 @@
 export default function createReportObject(employeesList) {
-	const allEmployees = {
-		...employeesList,
-	};
+  const allEmployees = {
+    ...employeesList,
+  };
 
-	const getNumberOfDepartments = (employeesList) => Object.keys(employeesList).length;
+  const getNumberOfDepartments = (employeesList) => Object.keys(employeesList).length;
 
-	return {
-		allEmployees,
-		getNumberOfDepartments,
-	};
+  return {
+    allEmployees,
+    getNumberOfDepartments,
+  };
 }
