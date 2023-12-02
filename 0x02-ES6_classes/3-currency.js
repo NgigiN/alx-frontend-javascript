3d28cf28d53f8f7d144464dcbf1b715de6488cb3
NgigiN <ngigi.nyongo@gmail.com>
2023-12-02 21:37:36 +0300

    adding file-3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  set code(cd) {
+    if (typeof cd !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = cd;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(nm) {
+    if (typeof nm !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = nm;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
