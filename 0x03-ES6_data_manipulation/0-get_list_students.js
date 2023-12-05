5a5ccfde5d0909ccaf434837be4d764519e0be75
NgigiN <ngigi.nyongo@gmail.com>
2023-12-05 09:47:49 +0300

    correcting file-0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,7 @@
 export default function getListStudents() {
-  class Students {
-    constructor(id, firstName, location) {
-      this.id = id;
-      this.firstName = firstName;
-      this.location = location;
-    }
-  }
-
-  const array = [new Students(1, 'Guillame', 'San Francisco'), new Students(2, 'James', 'Columbia'), new Students(5, 'Serena', 'San Francisco')];
-
-  return array;
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
