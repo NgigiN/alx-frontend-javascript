ef1e439e86e67b9e26e4cf4ec2c06b21f705c4f4
NgigiN <ngigi.nyongo@gmail.com>
2023-12-05 10:08:23 +0300

    correcting file-5, to fix eslint errors and correctly set the array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,10 +2,8 @@ export default function create8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
-
   const buffer = new ArrayBuffer(length);
-  const int8Array = new int8Array(buffer);
-  int8Array[position] = value;
-
-  return buffer;
+  const int8Array = new DataView(buffer);
+  int8Array.setInt8(position, value);
+  return int8Array;
 }
