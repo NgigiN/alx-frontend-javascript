1abe05edfdf868ac96c99d54d2f6a46179d20be1
NgigiN <ngigi.nyongo@gmail.com>
2023-12-05 10:05:19 +0300

    fixing file-4 to take object parameters and correctly map the results

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,8 @@
-export default function updateStudentGradeByCity(student, city, ...newGrades) {
-  const students = student.filter((student) => student.location === city);
-  const ids = students.map((student) => student.id);
-  const newStudents = student.map((student) => {
-    if (ids.includes(student.id)) {
-      const grade = newGrades.filter((grade) => grade.studentId === student.id);
-      return { ...student, grade: grade[0] ? grade[0].grade : 'N/A' };
-    }
-    return student;
-  });
-  return newStudents;
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const grade = newGrades.find((g) => g.studentId === student.id);
+      return { ...student, grade: (grade && grade.grade) || 'N/A' };
+    });
 }
