f01bd584e32211c5f3490b1d55b263999be01018
NgigiN <ngigi.nyongo@gmail.com>
2023-12-13 17:36:54 +0300

    adding folder-1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,7 @@
+'use strict';
+exports.__esModule = true;
+exports.printTeacher = void 0;
+const printTeacher = function (firstName, lastName) {
+  return firstName[0] + '. ' + lastName;
+};
+exports.printTeacher = printTeacher;
